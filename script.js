5211dbb4e6a9089dbf7a3e710524619f364c68b8
jsndz <jaisondz9360.com>
2024-02-10 01:09:19 +0530

    BAsic animation setup

---
+++ script.js
@@ -0,0 +1,101 @@
+let playerState = "idle";
+const dropdown = document.getElementById("animations");
+dropdown.addEventListener("change", (e) => {
+  playerState = e.target.value;
+});
+
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+
+const CANVAS_WIDTH = (canvas.width = 600);
+const CANVAS_HEIGHT = (canvas.height = 600);
+
+const playerImage = new Image();
+playerImage.src = "shadow_dog.png";
+const spritewidth = 575;
+const spriteheight = 523;
+
+let gameFrame = 0;
+let staggerFrame = 5;
+
+const spriteAnimation = [];
+const animationState = [
+  {
+    name: "idle",
+    frames: 7,
+  },
+  {
+    name: "jump",
+    frames: 7,
+  },
+  {
+    name: "fall",
+    frames: 7,
+  },
+  {
+    name: "run",
+    frames: 9,
+  },
+  {
+    name: "dizzy",
+    frames: 11,
+  },
+  {
+    name: "sit",
+    frames: 5,
+  },
+  {
+    name: "rool",
+    frames: 7,
+  },
+  {
+    name: "bite",
+    frames: 7,
+  },
+  {
+    name: "ko",
+    frames: 12,
+  },
+  {
+    name: "getHit",
+    frames: 4,
+  },
+];
+
+animationState.forEach((state, index) => {
+  let frames = {
+    loc: [],
+  };
+  for (let j = 0; j < state.frames; j++) {
+    let positionX = j * spritewidth;
+    let positionY = spriteheight * index;
+    frames.loc.push({ x: positionX, y: positionY });
+  }
+  spriteAnimation[state.name] = frames;
+});
+console.log(spriteAnimation);
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+  let position =
+    Math.floor(gameFrame / staggerFrame) %
+    spriteAnimation[playerState].loc.length;
+  frameX = spriteAnimation[playerState].loc[position].x;
+  frameY = spriteAnimation[playerState].loc[position].y;
+  ctx.drawImage(
+    playerImage,
+    frameX,
+    frameY,
+    spritewidth,
+    spriteheight,
+    0,
+    0,
+    spritewidth,
+    spriteheight
+  );
+
+  gameFrame++;
+  requestAnimationFrame(animate);
+}
+
+animate();
